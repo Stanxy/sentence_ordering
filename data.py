Eliminated logic bug. Debugging numerical bug and memory bugs.

data.py
@@ -62,7 +62,7 @@ def fetch_sentence_pairs(tokenizer, pairs, shuffled_index, passage, p_key):
     max_sample_len = 0 # save the max length of ids inorder to do padding
 
     for sent_id1, sent_id2 in pairs:
-        sep_position = (0,0)
+        sep_position = [0,0]
 
         sent1 = passage[shuffled_index[sent_id1]]
         sent2 = passage[shuffled_index[sent_id2]]
@@ -71,15 +71,15 @@ def fetch_sentence_pairs(tokenizer, pairs, shuffled_index, passage, p_key):
         concat_sents = ['[CLS]'] + tokenized_sent1 + ['[SEP]'] 
         sep_position[0] = len(concat_sents) - 1
         token_type_id = [0] * len(concat_sents)
-        concat_sents = tokenized_sent2 + ['[SEP]']
+        concat_sents += tokenized_sent2 + ['[SEP]']
         input_id = tokenizer.convert_tokens_to_ids(concat_sents)
         if len(concat_sents) > 512: # 
-            concat_Sents = concat_Sents[:512] # 
+            concat_sents = concat_sents[:512] # 
             print('SEQ TOO LONG! passage : {} pairs : {} {}'.format(p_key, \
-                shuffled_index[sent_id1], shuffled_index[sent_id1]))
+                shuffled_index[sent_id1], shuffled_index[sent_id2]))
 
         sep_position[1] = len(concat_sents) - 1
-        token_type_id = [1] * len(tokenized_sent2 + ['[SEP]'])
+        token_type_id += [1] * len(tokenized_sent2 + ['[SEP]'])
         masked_id = [1] * len(token_type_id)
 
         max_sample_len = len(masked_id) if max_sample_len < len(masked_id) else max_sample_len
@@ -87,7 +87,7 @@ def fetch_sentence_pairs(tokenizer, pairs, shuffled_index, passage, p_key):
         input_ids.append(input_id)
         masked_ids.append(masked_id)
         token_type_ids.append(token_type_id)
-        sep_positions.append(sep_position)
+        sep_positions.append(tuple(sep_position))
     
     return input_ids, token_type_ids, masked_ids, sep_positions, max_sample_len
 
@@ -107,7 +107,7 @@ def pairs_generator(lenth):
     combs = combs_one_side + combs_other_side
     return combs, len(combs)
 
-def convert_passage_to_sample_bundle(tokenizer, data: 'json refined', mode, kw, p_key):
+def convert_passage_to_samples_bundle(tokenizer, data: 'json refined', mode, kw, p_key):
     '''Make training samples.
         
         Convert document-format dict samples(various fields plus a list or long sting as the passage) to bundles.
@@ -126,14 +126,17 @@ def convert_passage_to_sample_bundle(tokenizer, data: 'json refined', mode, kw, 
         Returns:
             Bundle: A bundle containing the fields for each sample (including gold and shaffled sample).
     '''
-    if mode == 'list': # Then we need to clean data
+    if mode == 'string': # Then we need to clean data
         try:
             passage = data[kw].replace('\n',' ') # clean the '\n' char
+            
             passage = ' '.join(word_tokenize(passage)) # clean the long spaces
             passage = sent_tokenize(passage)
         except:
-            print ("Error when processing passage {}.\n".format(passage['id_'])) # print out which specific passage is dead
             print_exc()
+            exit()
+            print ("Error when processing passage {}.\n".format(passage['id'])) # print out which specific passage is dead
+            
     else:
         passage = data[kw]
 
@@ -165,9 +168,10 @@ def load_superbatch(data_file, super_batch_size): # we load the file by its supe
 
             if count_line % super_batch_size == 0 or count_line == line_num: # dump when a super is done
                 lines += line.strip() # push the last line into the lines stack
-                if lines.endswith(',\n'):
-                    lines = lines[:-2]
+                if lines.strip().endswith(','):
+                    lines = lines.strip()[:-1]
                 lines = '''[''' + lines + ''']''' # add 
+                # print (lines[:5], '\n', lines[:-5])
                 super_json_obj = json.loads(lines)
                 yield super_json_obj, line_num # yield a list
                 lines = "" # reinitialize lines
@@ -186,11 +190,11 @@ def homebrew_data_loader(bundles, batch_size = 8):
         (int, Generator): number of batches and a generator to generate batches.
     '''
 
-    bundles
+    # bundles
     if hasattr(bundles[0], 'ground_truth'): # check if we are training or predict
-        valid_field = 8
+        valid_field = 10
     else:
-        valid_field = 7
+        valid_field = 9
 
     # here we only shuffle passage
     n = len(bundles)
@@ -199,13 +203,16 @@ def homebrew_data_loader(bundles, batch_size = 8):
     np.random.permutation(bundles)
 
     all_bundle = Bundle() # all_bundle is a bundle object
-    for field in TRAIN_TEST_FIELDS[:valid_field]: # for the first 7 fields
+    for field in TRAIN_TEST_FIELDS[:valid_field]: # for the first few fields
         t = [] # 
-        setattr(all_bundle, field, t) # t is an empty list. This is to set a bundle where its first 7 fields are all empty lists. 
+        setattr(all_bundle, field, t) # t is an empty list. This is to set a bundle where its first few fields are all empty lists. 
         # seems like a kind of initialization
         for bundle in bundles: # iterate all bundle in bundles
-            t.extend(getattr(bundle, field)) # 
-        # In this step the fields in bundles are fill into an empty list 
+            if field not in set(['passage_length', 'pairs_num', 'max_sample_len','ground_truth','shuffled_index']):
+                t.extend(getattr(bundle, field)) # 
+            else:
+                t.append(getattr(bundle, field))
+        # In this step the fields in ,bundles are fill into an empty list 
     
     num_batch = (n - 1) // batch_size + 1
     def gen():

train.py
@@ -59,7 +59,7 @@ def train(training_data_file, valid_data_file, super_batch_size, tokenizer, mode
         {'params': [p for n, p in param_optimizer_others if not any(nd in n for nd in no_decay)], 'weight_decay': lambda_},
         {'params': [p for n, p in param_optimizer_others if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
         ]
-
+    # We shall adda  module to count the num of parameters here
     critic = nn.NLLLoss(reduction='none')
 
     line_num = int(os.popen("wc -l " + training_data_file).read().split()[0])
@@ -73,7 +73,7 @@ def train(training_data_file, valid_data_file, super_batch_size, tokenizer, mode
     model2.train() # 
     model3.to(device) # 
     model3.train() # 
-
+    warmed = True
     for epoch in trange(num_epoch, desc = 'Epoch'):
         
         smooth_mean = WindowMean()
@@ -99,13 +99,18 @@ def train(training_data_file, valid_data_file, super_batch_size, tokenizer, mode
                     #batch[0] = batch[0].to(device)
                     #batch[1] = batch[1].to(device)
                     #batch[2] = batch[2].to(device)
-                    batch = tuple(t for t in enumerate(batch))
+                    batch = tuple(t for t in batch)
                     log_prob_loss, pointers_output, ground_truth = calculate_loss(batch, model1, model2, model3, device, critic)
                     # here we need to add code to cal rouge-w and acc
-                    rouge_ws = rouge_w(pointers_output, ground_truth)
-                    accs = acc(pointers_output, ground_truth)
-                    ken_taus = kendall_tau(pointers_output, ground_truth)
-                    pmrs = pmr(pointers_output, ground_truth)
+                    rouge_ws = []
+                    accs = []
+                    ken_taus = []
+                    pmrs = []
+                    for pred, true in zip(pointers_output, ground_truth):
+                        rouge_ws.append(rouge_w(pred, true))
+                        accs.append(acc(pred, true))
+                        ken_taus.append(kendall_tau(pred, true))
+                        pmrs.append(pmr(pred, true))
 
                     log_prob_loss.backward()
 
@@ -122,7 +127,7 @@ def train(training_data_file, valid_data_file, super_batch_size, tokenizer, mode
                         opt2.zero_grad()
                         smooth_mean_loss = smooth_mean.update(log_prob_loss.item())
                         tqdm_obj.set_description('{}: {:.4f}, {}: {:.4f}, smooth_mean_loss: {:.4f}'.format(
-                            'accuracy', accs, 'rough-w', rouge_ws,  smooth_mean_loss))
+                            'accuracy', np.mean(accs), 'rough-w', np.mean(rouge_ws),  smooth_mean_loss))
                         # During warming period, model1 is frozen and model2 is trained to normal weights
                         if smooth_mean_loss < 1.0 and step > 100: # ugly manual hyperparam
                             warmed = True
@@ -138,6 +143,7 @@ def train(training_data_file, valid_data_file, super_batch_size, tokenizer, mode
 
                 except Exception as err:
                     traceback.print_exc()
+                    exit()
                     # if mode == 'list':   
                     #     print(batch._id) 
 
@@ -198,9 +204,9 @@ def train(training_data_file, valid_data_file, super_batch_size, tokenizer, mode
                 print('early stop at epc {}'.format(epoch))
                 break
 
-def main(output_model_file = './models/bert-base-cased.bin', training_data_file = './data/TRAIN_DATA_NAME', \
-        valid_data_file = './data/VALID_DATA_NAME', super_batch_size = 200, mode = 'list', kw = 'abstract', \
-        batch_size = 4, num_epoch = 1, gradient_accumulation_steps = 1, lr1 = 1e-4, lr2 = 1e-4, \
+def main(output_model_file = './models/bert-base-cased.bin', training_data_file = './sample/arxiv-abstract-train.json', \
+        valid_data_file = './sample/arxiv-abstract-dev.json', super_batch_size = 200, mode = 'list', kw = 'abstract', \
+        batch_size = 1, num_epoch = 1, gradient_accumulation_steps = 1, lr1 = 1e-4, lr2 = 1e-4, \
         lambda_ = 0.01, p_key = 'title', valid_critic = 'ken-tau', early_stop = 5, load=False):
     BERT_MODEL = 'bert-base-cased'
     tokenizer = BertTokenizer.from_pretrained(BERT_MODEL, do_lower_case=False)
@@ -209,28 +215,29 @@ def main(output_model_file = './models/bert-base-cased.bin', training_data_file 
         # Here if we have pretrained model we could use this
         print("Loading model from {}".format(output_model_file))
         model_state_dict = torch.load(output_model_file)
-        model1 = HierarchicalSentenceEncoder.from_pretrained(BERT_MODEL, state_dict=model_state_dict['params1'])
+        model1 = HierarchicalSentenceEncoder(BERT_MODEL, state_dict=model_state_dict['params1'])
         model2 = GlobalGraphPropagation(model1.config.hidden_size)
         model2.load_state_dict(model_state_dict['params2'])
-        model3 = BackwardFowardAttentiveDecoder(model1.config.hidden_size)
+        model3 = BackwardFowardAttentiveDecoder(model1.config)
         model2.load_state_dict(model_state_dict['params3'])
 
     else: # build new model
         # build sentence_encoder
-        model1 = HierarchicalSentenceEncoder.from_pretrained(BERT_MODEL,
+        model1 = HierarchicalSentenceEncoder(BERT_MODEL,
             cache_dir=PYTORCH_PRETRAINED_BERT_CACHE / 'distributed_{}'.format(-1))
         print ("Finish building model1")
         model2 = GlobalGraphPropagation(model1.config.hidden_size)
         print ("FInish building model2")
-        model3 = BackwardFowardAttentiveDecoder(model1.config.hidden_size)
-        print ("FInish building model2")
+        model3 = BackwardFowardAttentiveDecoder(model1.config)
+        print ("FInish building model3")
     
     print('Start Training... on {} GPUs'.format(torch.cuda.device_count()))
     model1 = torch.nn.DataParallel(model1, device_ids = range(torch.cuda.device_count()))
-    train(training_data_file, super_batch_size, tokenizer, mode, kw, p_key, \
-        model1=model1, device=device, model2=model2, model3=model3, # Then pass hyperparams
+    train(training_data_file, valid_data_file=valid_data_file, super_batch_size=super_batch_size, \
+        tokenizer=tokenizer, mode=mode, \
+        kw=kw, p_key=p_key, model1=model1, device=device, model2=model2, model3=model3, # Then pass hyperparams
         batch_size=batch_size, num_epoch=num_epoch, gradient_accumulation_steps=gradient_accumulation_steps, \
-            lr11=lr1, lr2=lr2, lambda_=lambda_, valid_critic = valid_critic, early_stop = early_stop) 
+            lr1=lr1, lr2=lr2, lambda_=lambda_, valid_critic = valid_critic, early_stop = early_stop) 
 
 #import fire
 if __name__ == "__main__":

utils.py
@@ -64,16 +64,19 @@ def bundle_part_to_batch(all_bundle, l = None, r = None):
     input_ids = torch.zeros((batch_length, max_length), dtype = torch.long)
     token_type_ids = torch.zeros((batch_length, max_length), dtype = torch.long)
     masked_ids = torch.zeros((batch_length, max_length), dtype = torch.long)
-    sep_positions = all_bundle.sep_positions[l:r]
+    
     ground_truth = all_bundle.ground_truth[l:r]
     passage_length = all_bundle.passage_length[l:r]
     pairs_num = all_bundle.pairs_num[l:r]
 
-    l_pairs = sum([x for x in all_bundle.pairs_num[:l]])
-    r_pairs = sum([x for x in all_bundle.pairs_num[:r]])
-    pairs_list = all_bundle.pairs_list[l_pairs, r_pairs]
+    l_pairs = sum(all_bundle.pairs_num[:l])
+    r_pairs = sum(all_bundle.pairs_num[:r])
+    pairs_list = all_bundle.pairs_list[l_pairs: r_pairs]
+
+    sep_positions = all_bundle.sep_positions[l_pairs:r_pairs]
+    
     for i in range(l_pairs,r_pairs):
-        length = len(all_bundle.ids[i])
+        length = len(all_bundle.input_ids[i])
         input_ids[i - l_pairs, :length] = torch.tensor(all_bundle.input_ids[i], dtype = torch.long)
         token_type_ids[i - l_pairs, :length] = torch.tensor(all_bundle.token_type_ids[i], dtype = torch.long)
         masked_ids[i - l_pairs, :length] = 1